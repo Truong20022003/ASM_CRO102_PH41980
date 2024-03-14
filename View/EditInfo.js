569520c4b9b40cf1b552a1d0d1057a7966da757f
Tran Quang Truong <quangtruong20022003@gmail.com>
2024-03-14 12:36:53 +0700

    first commit

---
+++ View/EditInfo.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const EditInfo = () => {
+  return (
+    <View>
+      <Text>EditInfo</Text>
+    </View>
+  )
+}
+
+export default EditInfo
+
+const styles = StyleSheet.create({})
